ab495fbd24412131bc2a4e0ba9f9904cec82cbff
FARSTBITER <237100840+FARSTBITER@users.noreply.github.com>
2026-03-01 20:53:02 +0200

    Added base FabricMC mod code&resources

---
+++ src/client/java/com/lmt/LMTClient.java
@@ -0,0 +1,12 @@
+package com.lmt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LMTClient implements ClientModInitializer {
+	
+	@Override
+	public void onInitializeClient() {
+		
+	}
+	
+}
---
+++ src/main/java/com/lmt/LMT.java
@@ -0,0 +1,14 @@
+package com.lmt;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LMT implements ModInitializer {
+	
+	public static final String MOD_ID = "lmt";
+	
+	@Override
+	public void onInitialize() {
+		
+	}
+	
+}
