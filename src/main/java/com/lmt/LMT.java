93b2f1d29b0e8af609a56cf84188281ebbf1e602
FARSTBITER <237100840+FARSTBITER@users.noreply.github.com>
2026-03-02 08:40:31 +0200

    Added furnaces fuel registry

--- src/main/java/com/lmt/LMT.java
+++ src/main/java/com/lmt/LMT.java
@@ -2,6 +2,7 @@ package com.lmt;
 
 import com.lmt.world.item.LMTCreativeModeTabs;
 import com.lmt.world.item.LMTItems;
+import com.lmt.world.level.block.entity.LMTFuelValues;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,7 @@ public class LMT implements ModInitializer {
 	public void onInitialize() {
 		
 		LMTItems.initialize();
+		LMTFuelValues.initialize();
 		LMTCreativeModeTabs.initialize();
 		
 	}
---
+++ src/main/java/com/lmt/world/level/block/entity/LMTFuelValues.java
@@ -0,0 +1,15 @@
+package com.lmt.world.level.block.entity;
+
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+
+public class LMTFuelValues {
+	
+	public static void initialize() {
+		
+		FuelRegistryEvents.BUILD.register((builder, context) -> {
+			
+		});
+		
+	}
+
+}
