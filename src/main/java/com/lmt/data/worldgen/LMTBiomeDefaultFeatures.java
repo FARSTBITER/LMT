72bc85e768f2b590b11301a21aaca3fb02757120
FARSTBITER <237100840+FARSTBITER@users.noreply.github.com>
2026-03-03 17:16:07 +0200

    Added niter ore generation

--- src/main/java/com/lmt/data/worldgen/LMTBiomeDefaultFeatures.java
+++ src/main/java/com/lmt/data/worldgen/LMTBiomeDefaultFeatures.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
 
 public class LMTBiomeDefaultFeatures {
@@ -12,6 +13,7 @@ public class LMTBiomeDefaultFeatures {
 	public static void initialize() {
 		
 		BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Decoration.UNDERGROUND_ORES, ResourceKey.create(Registries.PLACED_FEATURE, Identifier.fromNamespaceAndPath("lmt", "sulfur_ore_nether")));
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(Biomes.DESERT, Biomes.SAVANNA, Biomes.SAVANNA_PLATEAU, Biomes.WINDSWEPT_SAVANNA, Biomes.BADLANDS, Biomes.WOODED_BADLANDS, Biomes.ERODED_BADLANDS), GenerationStep.Decoration.UNDERGROUND_ORES, ResourceKey.create(Registries.PLACED_FEATURE, Identifier.fromNamespaceAndPath("lmt", "ore_niter")));
 		
 	}
 
